ad9cf40875fba3e5d5c88d199e1e1113bed90b4e
surajverma2587 <surajverma2587@gmail.com>
2022-05-07 12:40:48 +0100

    render notification

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,40 @@
-console.log("hello from index");
+const playlistContainer = $("#playlists");
+
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+const renderPlaylists = (playlists) => {};
+
+const renderNotification = (message) => {
+  // create component
+  const notification = `<div class="notification is-warning">${message}</div>`;
+
+  // append component to playlistContainer
+  playlistContainer.append(notification);
+};
+
+const onReady = () => {
+  // get playlists from LS
+  const playlists = readFromLocalStorage("playlists", []);
+
+  if (playlists.length) {
+    // render playlists
+    renderPlaylists(playlists);
+  } else {
+    // render notification
+    renderNotification("You have no playlists.");
+  }
+};
+
+$(document).ready(onReady);
