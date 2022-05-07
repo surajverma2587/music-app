04cb0e0e0c82a6ce10bd1aa0ef397ab6d36629de
surajverma2587 <surajverma2587@gmail.com>
2022-05-07 12:48:30 +0100

    conditionally render playlists in JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,14 +14,31 @@ const readFromLocalStorage = (key, defaultValue) => {
   }
 };
 
-const renderPlaylists = (playlists) => {};
+const renderPlaylists = (playlists) => {
+  const createPlaylist = (playlist) => {
+    return `<li class="panel-block">
+      <span class="panel-icon">
+        <i class="fas fa-music" aria-hidden="true"></i>
+      </span>
+      ${playlist}
+    </li>`;
+  };
+
+  // create playlists
+  const playlistsComponent = `<ul>
+    ${playlists.map(createPlaylist).join("")}
+  </ul>`;
+
+  // append playlists to playlistContainer
+  playlistContainer.append(playlistsComponent);
+};
 
 const renderNotification = (message) => {
   // create component
-  const notification = `<div class="notification is-warning">${message}</div>`;
+  const notificationComponent = `<div class="notification is-warning">${message}</div>`;
 
   // append component to playlistContainer
-  playlistContainer.append(notification);
+  playlistContainer.append(notificationComponent);
 };
 
 const onReady = () => {
